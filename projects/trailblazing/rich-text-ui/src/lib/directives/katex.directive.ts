4476a8509146e54f8038d7955577daea03d461f2
trailblazing-dev <thong@trailblazing.dev>
2021-12-16 10:15:49 -0800

    update jsdoc for the service and the directive

--- projects/trailblazing/rich-text-ui/src/lib/directives/katex.directive.ts
+++ projects/trailblazing/rich-text-ui/src/lib/directives/katex.directive.ts
@@ -7,14 +7,42 @@ import { KatexService } from "../services/katex.service";
 })
 export class KatexDirective implements OnChanges {
 
+  /**
+   * The default options for the rendering
+   * @private
+   */
   private defaultOptions: KatexOptions = { output: 'html' };
+
+  /**
+   * The raw content of the expression
+   */
   @Input() expression: string = '';
+
+  /**
+   * The additional options for the rendering
+   */
   @Input() options?: any = undefined;
+
+  /**
+   * Enable the debug mode in which errors will be rendered directly to the destination
+   */
   @Input() debug: boolean = false;
+
+  /**
+   * The event that will be emitted to the parent component in case of errors
+   */
   @Output() error = new EventEmitter<any>();
 
+  /**
+   *
+   * @param {ElementRef} el the target element (injected)
+   * @param {KatexService} service the katex service (injected)
+   */
   constructor(private el: ElementRef, private service: KatexService) { }
 
+  /**
+   * The internal processing on changes
+   */
   ngOnChanges() {
     try {
       this.el.nativeElement.classList.remove('has-error');
