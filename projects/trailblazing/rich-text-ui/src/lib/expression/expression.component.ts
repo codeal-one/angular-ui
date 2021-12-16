37cdafae0d9b1e3d7953bdd8164fa0444bb8d881
trailblazing-dev <thong@trailblazing.dev>
2021-12-15 22:13:58 -0800

    add jsdoc for expression and paragraph components

--- projects/trailblazing/rich-text-ui/src/lib/expression/expression.component.ts
+++ projects/trailblazing/rich-text-ui/src/lib/expression/expression.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, Input, Output, } from '@angular/core';
 
+/**
+ * This is mathematical expression component.
+ * The expression could be either inline within a paragraph or separated as a new paragraph
+ */
 @Component({
   selector: 'nt-expression',
   templateUrl: './expression.component.html',
@@ -7,13 +11,35 @@ import { Component, EventEmitter, Input, Output, } from '@angular/core';
 })
 export class ExpressionComponent  {
 
-  defaultOptions: any = { output: 'html' };
+  /**
+   * The default options for expression rendering
+   */
+  defaultOptions: any = { output: 'html', displayMode: false };
 
+  /**
+   * The content of the expression, excluding the delimiters such as `$`, `$$`, `\(`, `\)`, `\[` and `\]`.
+   */
   @Input() content: string = '';
-  @Input() options?: any = undefined;
-  @Input() debug = false;
-  @Output() error = new EventEmitter<any>(true);
 
+  /**
+   * The options for rendering the expression
+   */
+  @Input() options?: any = undefined;
+
+  /**
+   * Enabling debug mode in which the error message will be printed out in case of math syntax errors
+   */
+  @Input() debug = false;
+
+  /**
+   * The event that will be emitted in case of errors
+   */
+  @Output() error = new EventEmitter<any>();
+
+  /**
+   * The internal error handler of the expression. This handler will emit an event to parent component
+   * @param error describe the error in details with `toString`
+   */
   onError(error: any) {
     this.error.emit(error);
   }
